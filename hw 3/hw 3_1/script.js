2a9e9e328bceca45e3c4680f19d4eecc3dbfff7b
IrenSkSt <skiv.study@gmail.com>
2021-11-08 22:23:05 +0300

    new hw 3

--- hw 3/hw 3_1/script.js
+++ hw 3/hw 3_1/script.js
@@ -1,5 +1,5 @@
+// на базе классов
 // получаем с сервера список товаров
-
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 
 // Универсальная функция отправки/получения данных с сервером
@@ -44,207 +44,212 @@ function send(onError, onSuccess, url, method = 'GET', headers = {}, timeout = 1
     }
 
     xhr.send();
-    console.log(xhr); // для проверки
+    // console.log(xhr); // для проверки
 }
 
-let goods;
 
-send(
-    (err) => {
-        console.log(err.text)
-    },
-    (request) => {
-        goods = JSON.parse(request).map(good => ({ title: good.product_name, price: good.price })) // формируем массив из полученных данных
-        console.log(goods); // для проверки
+// Блок формирования Каталога товаров
+class GoodsItem { // карточка товара
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-        renderGoodsList(); // формирование карточек товаров
+    // метод отрисовывает карточку
+    render(i) {
 
-    },
-    `${API_URL}catalogData.json`
-)
-// console.log(goods); // для проверки
-
-
-
-// готовый код ----------/не мой/
-// class GoodsList {
-//     constructor() {
-//         this.goods = [];
-//         // console.log(goods); // для проверки
-//     }
-
-//     fetchGoods() {
-//         send(
-//             (err) => {
-//                 console.log(err.text)
-//             },
-//             (request) => {
-//                 this.goods = JSON.parse(request).map(good => ({ title: good.product_name, price: good.price }))
-//             }, // формируем массив из полученных данных
-//             `${API_URL}catalogData.json`
-//         )
-//         //console.log(goods); // для проверки
-//     }
-// }
-
-//------------------
-
-// вместо этого списка товаров подгружаем с сервера
-// let goods = [ // массив товаров магазина !!! Пока без остатков на складе - как бы по одному экземпляру =уникальные
-//     { title: 'Shirt', price: 150 },
-//     { title: 'Socks', price: 50 },
-//     { title: 'Jacket', price: 350 },
-//     { title: 'Shoes', price: 250 },
-// ];
-// console.log(goods); // для проверки
-
-// // const list = new GoodsList();
-// // list.fetchGoods();
-// // console.log(list); // для проверки
-// // const goods = list.goods;
-// // console.log(goods); // для проверки
-
-// Формируются карточки товаров в блоке товаров
-const $catalog = document.querySelector('.box_cards'); //блок товаров
-let i = 1;
-const renderGoodsItem = ({ title, price }) => {
-    return `<figure id="card_${i}" class="card"><h3>${title}</h3><p>Цена: ${price}</p><button id="btn_${i++}" class="add-cart">Купить</button></figure>`;
+        return `<figure id="card_${i}" class="card"><h3>${this.title}</h3><p>Цена: ${this.price}</p><button id="btn_${i++}" class="add-cart">Купить</button></figure>`;
+    }
 }
 
-const renderGoodsList = (list = goods) => {
-    let goodsCardsList = list.map(
-        (item) => {
-            return renderGoodsItem(item);
-        }
-    ).join('');
+class GoodsList { // массив = каталог из карточек товаров
+    constructor() {
+        this.goods = [];
+    }
 
-    $catalog.insertAdjacentHTML('beforeend', goodsCardsList);
+    fetchGoods() { // метод, в котором будем получать данные с сервера о товарах на складе
+        send(
+            (err) => {
+                console.log(err.text)
+            },
+            (request) => {
+                this.goods = JSON.parse(request).map(good => ({ title: good.product_name, price: good.price })) // формируем массив из полученных данных
+                // console.log(this.goods); // для проверки
+
+                this.render(); // формирование карточек товаров
+                this.checkSum(); // расчет стоимости товара на складе
+
+            },
+            `${API_URL}catalogData.json`
+        )
+        // console.log(goods); // для проверки
+
+        // this.goods = [ //  текущий каталог товаров на складе - заглушка вместо, полученной инфы с сервера
+        //     { title: 'Shirt', price: 150 },
+        //     { title: 'Socks', price: 50 },
+        //     { title: 'Jacket', price: 350 },
+        //     { title: 'Shoes', price: 250 },
+        // ]
+
+    }
+
+    render() { // метод отображает список товаров = католог карточек товаров
+
+        let listHtml = '';
+        let i = 1;
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render(i++);
+
+        });
+        document.querySelector('.box_cards').innerHTML = listHtml;
+
+    }
+
+    checkSum() { // рассчет общей стоимости товара на складе
+        let goodsSumCounter = 0;
+        this.goods.forEach(good => {
+            goodsSumCounter += good.price;
+        });
+        console.log(`Общая стоимость товара на складе = ${goodsSumCounter} долл.США`);
+    }
+
+
 }
 
-// renderGoodsList();
+// запуск формирования каталога на странице
+const catalog = new GoodsList();
+catalog.fetchGoods();
+// catalog.render();
+// catalog.checkSum();
+
+// console.log(catalog); // для проверки
+// console.log(catalog.goods); // для проверки
+//----------------------------------------
+
+// переделать в Контроллер
+// Перейти в корзину или Показать корзину по нажатия кнопки "Корзина" - повторное нажатие =скрыть
+let isOpenCart = false;
+function showCart() { // запускается по клику на кнопку Корзина 
+    // console.log(this); // для проверки
+    if (!isOpenCart) {
+        document.querySelector('.cart').style.display = 'block';
+        isOpenCart = true;
+    } else {
+        document.querySelector('.cart').style.display = 'none';
+        isOpenCart = false;
+    }
+}
+const openCart = document.querySelector('.cart-button').onclick = showCart; // открывается блок Корзины
+
+// на кнопку Купить
+function showBuy() { // запускается по клику на кнопку Купить 
+    // console.log(this); // для проверки
+    if (!isOpenCart) {
+        document.querySelector('.cart').style.display = 'block';
+        isOpenCart = true;
+    }
+}
 //-----------------------------------------------------
 
 
-// //++++ Добавлен расчет общей стоимости товара на складе 
-// const priceList = goods.map((item) => {
-//     // console.log(item.price); // для проверки
-//     return item.price;
-// });
+//Блок формирования КОрзины по кнопке Купить
+let Buys = []; // массив покупок
+class BuysItem { // позиция по товару в корзине
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-// let goodsSumCounter;
-// function SumCounter(items) {
-//     goodsSumCounter = items.reduce((prev, cur) => {
-//         return prev + cur;
-//     })
-// };
-// // SumCounter(priceList);
+    // метод отрисовывает позицию товара в корзине
+    render() {
 
-// console.log("Общая стоимость товара на складе = " + goodsSumCounter + " долл.США");
-// //-----------------------------------------------------
+        return `<figure ><h3>${this.title}</h3> <p>1 шт. * ${this.price} долл.США = ${this.price} долл.США</p > </figure > `; // добавить потом кнопку удаления товара из корзины <button class="cart_delete">Удалить</button> + id="pos_${indexGood}"
+    }
+}
 
+class CartList { // массив = список купленных товаров
+    constructor() {
+        this.buys = Buys;
+    }
 
-// // Перейти в корзину или Показать корзину по нажатия кнопки "Корзина" - повторное нажатие =скрыть
-// let isOpenCart = false;
-// function showCart() { // запускается по клику на кнопку Корзина 
-//     // console.log(this); // для проверки
-//     if (!isOpenCart) {
-//         document.querySelector('.cart').style.display = 'block';
-//         isOpenCart = true;
-//     } else {
-//         document.querySelector('.cart').style.display = 'none';
-//         isOpenCart = false;
-//     }
-// }
-// const openCart = document.querySelector('.cart-button').onclick = showCart; // открывается блок Корзины
+    // fetchCart() { // метод, в котором будем получать данные с сервера о корзине
+    //     this.buys = 
 
+    // }
 
-// // на кнопку Купить
-// function showBuy() { // запускается по клику на кнопку Купить 
-//     // console.log(this); // для проверки
-//     if (!isOpenCart) {
-//         document.querySelector('.cart').style.display = 'block';
-//         isOpenCart = true;
-//     }
-// }
-// //-----------------------------------------------------
+    addBuy(indexGood) {  // добавляем покупку в массив покупок
+        this.buys[indexGood] = catalog.goods[indexGood];
+        // Buys = this.buys;
+        // console.log(this.buys[indexGood].title); // для проверки
 
+    }
 
-// // Пользователь по клику на кнопку "Купить" добавляет товар в корзину
+    render() { // метод отображает список покупок
+        let listHtml = '';
+        this.buys.forEach(buy => {
+            const buyItem = new BuysItem(buy.title, buy.price);
+            listHtml += buyItem.render();
+        });
+        // document.querySelector('.cart').insertAdjacentHTML('beforeend', listHtml);
+        document.querySelector('.cart-list').innerHTML = listHtml;
+    }
 
-// const $cart = document.querySelector('.cart'); //блок корзины
-// const $cardsList = document.getElementsByClassName("card"); // коллекция карточек товаров
-// // console.log($cardsList); // для проверки
-// let cart = []; // пустая корзина
+    checkSum() { // рассчет общей стоимости покупок в корзине и отражение ее в корзине
+        let buysSumCounter = 0;
+        this.buys.forEach(buy => {
+            buysSumCounter += buy.price;
+        });
+        // console.log("Общая стоимость покупок в корзине = " + buysSumCounter + " долл.США"); // для проверки
+        let totalBuy = document.querySelector('p');
+        totalBuy.innerText = `Общая стоимость Ваших покупок составляет = ${buysSumCounter} `; // добавили стоимость всех покупок в корзине
+        totalBuy.innerHTML += "&#36;";
+        totalBuy.style.color = "blue";
+        // console.log(totalBuy); // для проверки
+    }
 
+}
 
-// //++++ Добавлен расчет общей стоимости товара в корзине  - аналогично как стоимость остатков на складе
-// let buySumCounter = 0;
-// function buySum(buys) {
-//     const coustList = buys.map((pos) => {
-//         // console.log(pos.price); // для проверки
-//         return pos.price;
-//     });
-//     function coustSum(pos) {
-//         buySumCounter = pos.reduce((prev, cur) => {
-//             return prev + cur;
-//         })
-//     };
-//     coustSum(coustList);
-// }
+// переделать в Контроллер
+// функция Клика на кнопку Кпить в карточке товара
+const $cardsList = document.getElementsByClassName("card"); // коллекция карточек товаров
+for (var item of $cardsList) {
 
-// var totalBuy = document.querySelector('p');
-// //-----------------------------------------------------
+    item.querySelector('.add-cart').onclick = function () {
+        // console.log(this); // для проверки
+        this.style.color = "brown"; //если товар добавили в корзину хоть один раз, то меняет цвет
 
+        n = this.id.split('_')[1]; // добавить проверки, на наличие на складе
+        //console.log(n); // для проверки
 
-// const renderCartPosition = ({ title, price }) => { // позиция покупки в блоке корзины
-//     // console.log(title, price); // для проверки
-//     return `<figure ><h3>${title}</h3> <p>1 шт. * ${price} долл.США = ${price} долл.США</p > </figure > `; // добавить потом кнопку удаления товара из корзины <button class="cart_delete">Удалить</button> + id="pos_${indexGood}"
-// }
-
-// function renderCartList(indexGood) {
-//     cart[indexGood] = goods[indexGood];
-//     //console.log(cart[indexGood].title); // для проверки
-//     buySum(cart);
-//     // console.log("Общая стоимость покупки = " + buySumCounter + " долл.США"); // для проверки
-//     totalBuy.innerText = "Общая стоимость Ваших покупок составляет = " + buySumCounter + " "; // добавили стоимость всех покупок в корзине
-//     totalBuy.innerHTML += "&#36;";
-//     totalBuy.style.color = "blue";
-//     // console.log(totalBuy); // для проверки
-
-//     return renderCartPosition(cart[indexGood], cart[indexGood]);
-
-// }
-// //--------------------------------------------------------------
+        if (Buys[n - 1] == null) { // маасив текущих покупок
+            // запуск по кнопке Купить
+            const cart = new CartList();
+            cart.addBuy(n - 1);
+            cart.render();
+            cart.checkSum();
 
 
+            // console.log(Buys); // для проверки
+            // console.log(cart.buys); // для проверки
+            // console.log(cart.buys[n - 1]); // для проверки
+
+        } else {
+            alert("Данный товар в единственном экземпляре. Вы уже добавили этот товар в корзину!");
+        }
+
+        showBuy(); // сразу открывается корзина по клику на кнопку Купить
+
+    };
+
+}
+//---------------------------------------------------
 
 
-// for (var item of $cardsList) {
 
-//     item.querySelector('.add-cart').onclick = function () {
-//         // console.log(this); // для проверки
-//         this.style.color = "brown"; //если товар добавили в корзину хоть один раз, то меняет цвет
 
-//         n = this.id.split('_')[1]; // добавить проверки, на наличие на складе
-//         //console.log(n); // для проверки
 
-//         if (cart[n - 1] == null) { // маасив покупок
-//             //renderCartList(n - 1); // добавляем покупку в массив покупок
-//             $cart.insertAdjacentHTML('beforeend', renderCartList(n - 1));
-//             //console.log(cartPos[n - 1]); // для проверки
 
-//         } else {
-//             alert("Данный товар в единственном экземпляре. Вы уже добавили этот товар в корзину!");
-//         }
-
-//         showBuy(); // сразу открывается корзина по клику на кнопку Купить
-
-//     };
-
-// }
-
-// // console.log(cart); // для проверки
-// //-----------------------------------------------------------------
 
 
 
